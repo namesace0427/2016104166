8455bdfb7d0df8d9ddc85c6bbf376b06e57f4702
kimsunghwan <wjswp12347@khu.ac.kr>
2021-10-08 10:30:46 +0900

    8 -> 9

--- gugudan.cpp
+++ gugudan.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
  
 int main() {
-    for (int i = 1; i <= 8; i++) {  // 8을 9로 변경 하면 됨
+    for (int i = 1; i <= 9; i++) {  // 8을 9로 변경 하면 됨
         for (int j = 1; j <= 9; j++) {
             printf("%3d x %3d = %2d", j, i, (i*j));
         }
