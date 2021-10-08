bc5ea3a8f9b1cfaa783e9c58136c24a67bd4edb7
namesace0427 <72364782+namesace0427@users.noreply.github.com>
2021-10-08 10:13:01 +0900

    구구단 8단 커밋
    구구단 8단 커밋

---
+++ gugudan.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    for (int i = 1; i <= 8; i++) {  // 8을 9로 변경 하면 됨
+        for (int j = 1; j <= 9; j++) {
+            printf("%3d x %3d = %2d", j, i, (i*j));
+        }
+        printf("\n");
+    }
+ 
+    return 0;
+}
